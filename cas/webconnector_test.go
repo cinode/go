816ea05366660005da22bae69613d6e18ec295d0
Bartek <byo.spoon@gmail.com>
2016-07-14 23:12:19 +0200

    Add web connector test

---
+++ cas/webconnector_test.go
@@ -0,0 +1,65 @@
+package cas
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+const (
+	webConnectorTestBlobName = "ZZ8FaUwURAkWvzbnRhTt2pWSJCYZMAELqPk9USTUJgC4"
+)
+
+func TestWebConnectorInvalidUrl(t *testing.T) {
+	c := FromWeb("://bad.url", &http.Client{})
+	_, err := c.Open("test")
+	if err == nil {
+		t.Fatal("Did not get error for Open")
+	}
+	_, err = c.Exists("test")
+	if err == nil {
+		t.Fatal("Did not get error for Exists")
+	}
+	err = c.Delete("test")
+	if err == nil {
+		t.Fatal("Did not get error for Delete")
+	}
+	err = c.Save(webConnectorTestBlobName, testData())
+	if err == nil {
+		t.Fatal("Did not get error for Delete")
+	}
+	_, err = c.SaveAutoNamed(testData())
+	if err == nil {
+		t.Fatal("Did not get error for Delete")
+	}
+}
+
+func TestWebConnectorServerSideError(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Error", http.StatusInternalServerError)
+	}))
+	defer server.Close()
+
+	c := FromWeb(server.URL+"/", &http.Client{})
+
+	_, err := c.Open("test")
+	if err == nil {
+		t.Fatal("Did not get error for Open")
+	}
+	_, err = c.Exists("test")
+	if err == nil {
+		t.Fatal("Did not get error for Exists")
+	}
+	err = c.Delete("test")
+	if err == nil {
+		t.Fatal("Did not get error for Delete")
+	}
+	err = c.Save(webConnectorTestBlobName, testData())
+	if err == nil {
+		t.Fatal("Did not get error for Delete")
+	}
+	_, err = c.SaveAutoNamed(testData())
+	if err == nil {
+		t.Fatal("Did not get error for Delete")
+	}
+}
